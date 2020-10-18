bc22b2d98e0264d8ee7a8855d93f6c30d3ca02d3
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2020-10-18 17:38:24 -0400

    index.js exporting the schemas

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-/*const User = require('./models/users.js');
+const User = require('./models/users.js');
 const Restaurant = require('./models/restaurants.js');
 const Menu = require('./models/menus.js');
 const MenuItem = require('./models/menuItems.js');
 const Comment = require('./models/comments.js');
 const Deliverer = require('./models/deliverers.js');
-const Order = require('./models/orders.js');*/
+const Order = require('./models/orders.js');
 
 module.exports = {
-	User: "User",
-	Restaurant: "Restaurant",
-	Menu: "Menu",
-	MenuItem: "MenuItem",
-	Comment: "Comment",
-	Deliverer: "Deliverer",
-	Order: "Order",
+	User,
+	Restaurant,
+	Menu,
+	MenuItem,
+	Comment,
+	Deliverer,
+	Order,
 };
+
