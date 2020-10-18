0bdcb2823fa8c8881b3fad049090e4f8932a0c74
Henry J. Perez <henryjosiasperezrangel@gmail.com>
2020-10-18 18:07:16 -0400

    index.js exporting the schemas 3

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const User = require('./models/users.js');
-const Restaurant = require('./models/restaurants.js');
-const Menu = require('./models/menus.js');
-const MenuItem = require('./models/menuItems.js');
-const Comment = require('./models/comments.js');
-const Deliverer = require('./models/deliverers.js');
-const Order = require('./models/orders.js');
-
-exports.User;
-exports.Restaurant;
-exports.Menu;
-exports.MenuItem;
-exports.Comment;
-exports.Deliverer;
-exports.Order;
-
-console.log("mamterazzi-mongodb is on")
+exports.User = require('./models/users.js');
+exports.Restaurant = require('./models/restaurants.js');
+exports.Menu = require('./models/menus.js');
+exports.MenuItem = require('./models/menuItems.js');
+exports.Comment = require('./models/comments.js');
+exports.Deliverer = require('./models/deliverers.js');
+exports.Order = require('./models/orders.js');
